839a723ca2eeafc6f5689c6fdf7abdc02ead022b
Osnielky <osnielkisrq@gmail.com>
2023-03-14 17:05:57 -0400

    Thre elemnets in the view

--- src/App.js
+++ src/App.js
@@ -1,66 +1,87 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import Procedure from './components/procedure';
+import FullName from './components/fulname/fullname';
 function App() {
 
   const procedures = ['Brazilian Bud Lift ', 'Tummy Tuck', 'Breast Lift ', 'Breast Augmentation']
   const howsoon = ['ASAP', 'One to Three Month', 'In six months ', 'In a Year']
-  const [show, setShow] = useState({ procedure: false, howsoonShow: true });
+  const [show, setShow] = useState({ chooseProcedureHidden: false, howsoonHidden: true, fullHameHidden: true });
+  let evaluation = useRef({ procedure: '', howSoon: '', FullName: '' })
 
-  const evaluation = {
-    procedure: '',
-    fullname: '',
-    telephone: '',
-    email: '',
-    howsoon: '',
-
-  }
   useEffect(() => {
     console.log(show);
-   
+
   }, [show]);
 
-  const setHowSoon = (howsoon) => {
-
-    evaluation.howsoon = howsoon;
-    console.log(evaluation);
-    setShow({
-      ...show,
-      howsoonShow: true,
-
-    },);
-
-  }
 
   const setProcedure = (procedureName) => {
 
-    evaluation.procedure = procedureName;
-    console.log(evaluation);
-    setShow({
-      ...show,
-      procedure: true,
-      
-    }, );
+    if (show.chooseProcedureHidden === false) {
 
+      const varEval = evaluation.current
+      evaluation.current = {
+        ...varEval,
+        procedure: procedureName
+      }
+      setShow({
+        ...show,
+        howsoonHidden: false,
+        chooseProcedureHidden: true,
+      },);
+      console.log(evaluation);
+    }
+
+    else if (show.howsoonHidden === false) {
+
+      const varEval = evaluation.current
+      evaluation.current = {
+        ...varEval,
+        howSoon: procedureName
+      }
+      console.log(evaluation);
+      setShow({
+        ...show,
+        howsoonHidden: true,
+        fullHameHidden: false,
+
+      },);
+
+    }
+    else if (show.fullHameHidden === false) {
+
+      const varEval = evaluation.current
+      evaluation.current = {
+        ...varEval,
+        FullName: procedureName
+      }
+      console.log(evaluation);
+      setShow({
+        ...show,
+        howsoonHidden: true,
+        fullHameHidden: true,
+
+      },);
+
+    }
   }
   return (
     <div className="App">
       {procedures.map(
         (item, i) => (
-          <Procedure show= {show.procedure} key={item + i} name={item} setProcedure={setProcedure}> </Procedure>
+          <Procedure show={show.chooseProcedureHidden} key={item + i} name={item} setProcedure={setProcedure}> </Procedure>
         )
       )}
 
       {howsoon.map(
         (item, i) => (
-          <Procedure show={show.howsoonShow} key={item + i} name={item} setProcedure={setProcedure}> </Procedure>
+          <Procedure show={show.howsoonHidden} key={item + i} name={item} setProcedure={setProcedure}> </Procedure>
         )
       )}
 
-
-
+      <FullName hidden={show.fullHameHidden} key='11111' setProcedure={setProcedure}></FullName>
 
     </div>
   );
---
+++ src/components/fulname/fullname.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material/';
+import './FullName.css'
+
+export default function Fullname(props) {
+  const [message, setMessage] = useState('');
+  const handleChange = (event) => {
+    // 👇 Get input value from "event"
+    setMessage(event.target.value);
+  };
+
+  return (
+    <div hidden={props.hidden} className='FullName'>
+
+      <TextField  label={'props.name'} onChange={handleChange} color="primary" focused />
+      <Button onClick={() => props.setProcedure (message)}  className='button' variant="contained">Continue</Button>
+
+    </div>
+  );
+} 
